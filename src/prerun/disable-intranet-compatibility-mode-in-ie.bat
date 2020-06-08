45dd8643dd246e74faf73f98af2836dad70197a1
Sebastian Chece <Sebastian.Chece@otto.de>
2020-06-08 15:13:34 +0200

    remove source files

--- src/prerun/disable-intranet-compatibility-mode-in-ie.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\BrowserEmulation" /t REG_DWORD /v IntranetCompatibilityMode /d 0 /f
--- src/utils/wait.js
+++
@@ -1,8 +0,0 @@
-import Promise from 'pinkie';
-
-
-export default function (ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
-}
